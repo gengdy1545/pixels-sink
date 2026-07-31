01761dd4626ca449d23896ff1de9c4f7a63d651c
Dongyang Geng <gengdy.ruc@gmail.com>
2026-07-31 23:56:55 +0800

    chore: unify Java license headers to Apache-2.0

--- src/test/java/io/pixelsdb/pixels/sink/TestUtils.java
+++ src/test/java/io/pixelsdb/pixels/sink/TestUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 PixelsDB.
+ * Copyright 2023 PixelsDB.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-
 package io.pixelsdb.pixels.sink;
 
 import java.util.concurrent.ExecutorService;
